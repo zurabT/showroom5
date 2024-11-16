1dc88d1a09c874c217f961800af2e918dca95293
zurabT <157989526+zurabT@users.noreply.github.com>
2024-11-17 00:28:14 +0400

    added home-link to logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,30 @@
 import logo from "../pictures/IMG-LOGO.jpg";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import { Link } from "react-router-dom";
 import Gallery from "./Gallery";
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate("/"); // Navigate to home page
+  };
   return (
     <div className="navbar">
-      <Link to="/" className="navbar-logo-link">
-        <img className="navbar-logo" src={logo} width="130" height="130" />
-      </Link>
+      {/* <Link to="/" className="navbar-logo-link"> */}
+      <img
+        onClick={handleLogoClick}
+        className="navbar-logo"
+        src={logo}
+        width="130"
+        height="130"
+      />
+      {/* </Link> */}
 
       <ul className="navbar-items">
         <li className="navbar-item">
